4556a3e8cc6d2b847c2f2ebef220eb29bd6fa990
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-17 20:18:48 +0200

    add bed and user choice for moving the bed

--- wednesday.js
+++ wednesday.js
@@ -1,25 +1,43 @@
 let itemsToMove = document.querySelectorAll('.itemsToMove li');
 let ulToMove = document.querySelector('ul.itemsToMove');
 let ulInside = document.querySelector('ul.itemsInside');
-
+let bed = document.querySelector('.bed');
+let ask = document.querySelector('.ask');
 let hint= document.querySelector('.hint p');
 itemsToMove.forEach(clicked);
 function clicked(li, index){
+    ask.className = "ask"; // clear hint for bed
     li.addEventListener('click', checkIndexAndMove);
     function checkIndexAndMove(){
-        if (index !=0){
+        if (index != 0){
             hint.textContent = "you can't move me";
             li.style.backgroundColor = "black";
-        } else if(index ==0) {
-            ulInside.appendChild(li);
+        } else if(index == 0) {
+            ulInside.insertBefore(li, ulInside.firstChild); // so the first suitecase moved is at the bottom in the elevator and the later ones are on top of the previous one
             li.style.backgroundColor ="transparent";
-            let itemsToMove = document.querySelectorAll('.itemsToMove li');
-            itemsToMove.forEach(clicked);
-            itemsToMove.forEach(changeToBlank);
+            let itemsToMove = document.querySelectorAll('.itemsToMove li'); //update index value
+            itemsToMove.forEach(clicked); //repeat the check of order and move top element
+            itemsToMove.forEach(changeToBlank); //remove error hint and color
             function changeToBlank(li){
                 li.style.backgroundColor = "transparent";
             }
             hint.textContent =" ";
         }
+    }
+}
+bed.addEventListener('click', areYouSure);
+function areYouSure(){
+    ask.className = "ask show";
+    let yes = document.querySelector('span');
+    yes.addEventListener('click', moveBed);
+    function moveBed(){
+        ask.className = "ask";
+        bed.style.transform = "translate(259px, -60px)";
+    }
+    bed.addEventListener('transitionend', tiltBed);
+    function tiltBed(){
+        bed.removeEventListener('animationend',moveBed);
+        bed.style.transition = "all .2s";
+        bed.style.transform = "translate(259px, -60px) rotate(7deg)";
     }
 }
