c70eb4751cb8196212aed4ef0aad69fc8e8338a2
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-18 14:47:44 +0200

    fixed something, but found bug if click no then click yes at moving bed !!!!!!!

--- wednesday.js
+++ wednesday.js
@@ -3,8 +3,6 @@ let chuSound = document.querySelector('#chu');
 let intro = document.querySelector('div.intro');
 let closeIntro = document.querySelector('button.closeIntro');
 // add links to storyboard and diagram on the page
-// stop music when everything is inside elevator
-// elevator shuts
 // add elevator button
 let halfDoor = document.querySelectorAll('.halfDoor');
 let humanWrapper = document.querySelector('.humanWrapper');
@@ -79,68 +77,69 @@ function closeI(){
         let bedPosition = bed.style.transform;
         if (!bedPosition){
             areYouSure();
-        } else {
-            hint.textContent = "you want to take me out again? I won\'t leave!";
-        }
-    }
-    function areYouSure(){
-        ask.className = "ask show";
-        let yes = document.querySelector('span');
-        yes.addEventListener('click', moveBed);
-        function moveBed(){
-            ask.className = "ask"; // hide ask div
-            human.style.display = "none";
-            humanL.style.display = "none";
-            humanR.style.display = "inherit";
-            humanR.style.transform = "translate(-400px, 0)"; //move human to the bed side
-            humanWrapper.className = "humanWrapper pushBed";
-            hint.textContent = "nice work~";
-            bed.style.transform = "translate(228px, -77px)";
-            bed.addEventListener('transitionend', tiltBed);
-            function tiltBed(){
-                bed.removeEventListener('animationend',moveBed);
-                bed.style.transition = "all .2s";
-                bed.style.transform = "translate(228px, -77px) rotate(17deg)";
-            }
-            let loseBloodBytime = setInterval(loseSomeBlood, 40);
-            let interval = 1;
-            function loseSomeBlood(){
-                if (currentBlood >3){
-                    currentBlood -=3;
-                    interval ++;
-                    if (interval == 50){
-                        clearInterval(loseBloodBytime);
-                        checkIfSuitcasesIn();
-                        function checkIfSuitcasesIn(){
-                            let itemsInEle = document.querySelectorAll('.itemsInside li');
-                            if (itemsInEle.length ==4){
-                                setTimeout(everythingIn2, 500);
-                                function everythingIn2(){
-                                    hint.textContent = "Thank you ! It's the right order to move the stuff, so you get to take the elevator as well~";
-                                    halfDoor.forEach(shut);
-                                    function shut(hD){
-                                        hD.style.transform = "rotateY(180deg)";
+            function areYouSure(){
+                ask.className = "ask show";
+                let yes = document.querySelector('span');
+                yes.addEventListener('click', moveBed);
+                function moveBed(){
+                    ask.className = "ask"; // hide ask div
+                    human.style.display = "none";
+                    humanL.style.display = "none";
+                    humanR.style.display = "inherit";
+                    humanR.style.transform = "translate(-400px, 0)"; //move human to the bed side
+                    humanWrapper.className = "humanWrapper pushBed";
+                    hint.textContent = "nice work~";
+                    bed.style.transform = "translate(228px, -77px)";
+                    bed.addEventListener('transitionend', tiltBed);
+                    function tiltBed(){
+                        bed.removeEventListener('animationend',moveBed);
+                        bed.style.transition = "all .2s";
+                        bed.style.transform = "translate(228px, -77px) rotate(17deg)";
+                    }
+                    let loseBloodBytime = setInterval(loseSomeBlood, 40);
+                    let interval = 1;
+                    function loseSomeBlood(){
+                        if (currentBlood >3){
+                            currentBlood -=3;
+                            interval ++;
+                            if (interval == 50){
+                                clearInterval(loseBloodBytime);
+                                checkIfSuitcasesIn();
+                                function checkIfSuitcasesIn(){
+                                    let itemsInEle = document.querySelectorAll('.itemsInside li');
+                                    if (itemsInEle.length ==4){
+                                        setTimeout(everythingIn2, 500);
+                                        function everythingIn2(){
+                                            hint.textContent = "Thank you ! It's the right order to move the stuff, so you get to take the elevator as well~";
+                                            halfDoor.forEach(shut);
+                                            function shut(hD){
+                                                hD.style.transform = "rotateY(180deg)";
+                                            }
+                                        }
                                     }
                                 }
                             }
+                        } else {
+                            humanR.style.transform ="translate(-400px, 110px) rotateX(87deg)";
+                            setTimeout(hintDead, 500);
+                            function hintDead(){
+                                hint.textContent = "Sorry, you worked too hard...You should have wait and recover more before you move the bed. I told you you need to be careful with the moving...  Refresh page to start over~ Be patient this time and see what happens~";
+                            }
+                            blood.style.display = "none";
+                            document.addEventListener('click', null); // so that after death no click will trigger anything else,like hint
+                            interval = 51; // so that the success alternative doesn't show up
                         }
                     }
-                } else {
-                    humanR.style.transform ="translate(-400px, 110px) rotateX(87deg)";
-                    setTimeout(hintDead, 500);
-                    function hintDead(){
-                        hint.textContent = "Sorry, you worked too hard...You should have wait and recover more before you move the bed. I told you you need to be careful with the moving...  Refresh page to start over~ Be patient this time and see what happens~";
-                    }
-                    blood.style.display = "none";
-                    document.addEventListener('click', null); // so that after death no click will trigger anything else,like hint
-                    interval = 51; // so that the success alternative doesn't show up
+                }
+                let no = document.querySelectorAll('span')[1];
+                no.addEventListener('click', hideAsk);
+                function hideAsk(){
+                    ask.className = "ask";
+                    hint.textContent = "fine, choose another stuff then"
                 }
             }
-        }
-        let no = document.querySelectorAll('span')[1];
-        no.addEventListener('click', hideAsk);
-        function hideAsk(){
-            ask.className = "ask";
+        } else {
+            hint.textContent = "you want to take me out again? I won\'t leave!";
         }
     }
     setInterval(recover, 100);
@@ -158,13 +157,15 @@ function closeI(){
             hD.style.transform = "rotateY(180deg)";
         }
         hint.textContent = "Thank you ! But it's not smart to throw the suitcases. Now you can take the stairs yourself. See you on the 7th floor~ 7th! ;)))";
-        strengthLevel.textContent = "angry level";
-        blood.style.backgroundColor = "red";
+        ulInside.style.transform = "translateY(-400px)";
+        ulInside.style.transition = "all .5s ease-in";
         human.style.display = "inherit";
         humanL.style.display = "none";
         humanR.style.display = "none";
         setTimeout(enlarge, 2500);
         function enlarge(){
+            strengthLevel.textContent = "angry level";
+            blood.style.backgroundColor = "red";
             human.style.transform = "scale(2)";
             human.style.transition = "all 3s ease-in";
             currentBlood += 30;
